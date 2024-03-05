cd483087034749f24fafbb62e3d32b41983b96bd
Abhishek Gupta <bsaagupta77@gmail.com>
2024-03-05 10:12:59 +0530

    added body parser

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const { PORT } = require('./config/serverConfig');
+
+
+const setupAndStartServer =  async () => {
+
+    // create express server
+    const app = express();
+
+    app.use(bodyParser.json());//middleware
+    app.use(bodyParser.urlencoded({extended: true}));//middleware
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`);
+    });
+
+}
+
+setupAndStartServer();
